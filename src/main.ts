7c84c63f769bec864227e69bb06384dd4a074d7d
R-unic <roleyspersonal@gmail.com>
2023-06-13 03:28:36 -0700

    fix: intents

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,24 @@
-import { Client } from "discord.js";
+import { Client, IntentsBitField } from "discord.js";
 import { configDotenv as configEnv } from "dotenv";
 import { exec } from "child_process";
 import * as fs from "fs";
 
 configEnv();
 const client = new Client({
-  intents: ["MessageContent"]
+  intents: [
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.MessageContent
+  ]
 });
 
 client.on("ready", () => console.log("Application online!"));
 client.on("error", console.error);
+
 client.on("messageCreate", message => {
   const args = message.content.split(" ");
   const command = args.shift();
-  if (command != "$cosmo") return;
+  if (command?.toLowerCase() != "$cosmo") return;
 
   const codeblock = args.join(" ");
   if (!codeblock.startsWith("```") || !codeblock.endsWith("```")) {
@@ -30,3 +35,5 @@ client.on("messageCreate", message => {
   const proc = exec("cosmo main.⭐");
   message.reply(`Output: ${proc.stderr?.read() || proc.stdout?.read()}`)
 });
+
+client.login(process.env.TOKEN);
