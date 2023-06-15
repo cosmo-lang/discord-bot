1f849332907567727dc43cedb452198fd1c6d5aa
R-unic <roleyspersonal@gmail.com>
2023-06-14 17:03:51 -0700

    feat: more helpful help menu

--- src/helpCommand.ts
+++ src/helpCommand.ts
@@ -1,11 +1,24 @@
 import { EmbedBuilder, Message } from "discord.js";
 
+const description = `
+This bot has exactly one command. That command is \`$cosmo\`. Here is some information about it:
+- It is to be executed with a code block.
+- You must include a space between \`$cosmo\` and the codeblock.
+- You must add a new line after the first triple backtick (\`\`\`).
+- The codeblock must use the [triple backtick (\`\`\`) syntax](https://www.technipages.com/discord-code-blocks/#attachment_55311).
+- Example:
+
+$cosmo \`\`\`
+puts("Hello, world!")
+\`\`\`
+`
+
 export function helpCommand(message: Message<boolean>) {
   message.reply({
     embeds: [
       new EmbedBuilder()
         .setTitle("Help Menu")
-        .setDescription("This bot has exactly one command. That command is `$cosmo`. The `$cosmo` command is to be executed with a code block. The given code block will then be interpreted by Cosmo. For example:\n\n$cosmo ```\nmy code here\n```")
+        .setDescription(description)
         .setColor("Blue")
         .setTimestamp()
     ]
